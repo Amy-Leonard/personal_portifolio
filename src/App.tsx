021a57b647ab7fc965423f96728fc573af5e3a0b
Thomas B <thblaauw@gmail.com>
2025-10-02 18:59:14 -0700

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Home from './pages/Home';
 import OneShots from './pages/OneShots';
 import CoolStuff from './pages/CoolStuff';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-
   return (
     <Router>
+      <Header />
+      
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/oneshots" element={<OneShots/>} />
-        <Route path="/coolstuff" element={<CoolStuff/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/oneshots" element={<OneShots />} />
+        <Route path="/coolstuff" element={<CoolStuff />} />
       </Routes>
+      
+      <Footer />
     </Router>
   );
-
-    return (
-      <div>
-        <Header/>
-        <Home/>
-        <Footer/>
-      </div>
-      )
-  
-  
- 
 }
 
 export default App;
